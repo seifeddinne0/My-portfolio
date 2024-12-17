2e732b9cdef2fb2ae204e7e7dd22ad1a1753657c
seif eddinne <159828511+seifeddinne0@users.noreply.github.com>
2024-12-17 01:03:49 +0100

    changing the title

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -6,7 +6,7 @@ function About(){
             <div className="d-flex align-items-center justify-content-center ">
             <hr className="flex-grow-1 border-white"/> <h1 className="mx-3 text-center text-danger">About</h1><hr className="flex-grow-1 border-white"/>
             </div>
-          <p className="text-center text-white h1-wrap-balance mx-auto">I’m a Computer science student with a deep enthusiasm for technology and hands-on experience in front-end development. I enjoy bringing creative designs to life on the web and exploring data to uncover insights. With a background in robotics and experience with Arduino, I’m always eager to learn new technologies and take on challenges that push my boundaries.</p>
+          <p className="text-center text-white h1-wrap-balance mx-auto h6">I’m a Computer science student with a deep enthusiasm for technology and hands-on experience in front-end development. I enjoy bringing creative designs to life on the web and exploring data to uncover insights. With a background in robotics and experience with Arduino, I’m always eager to learn new technologies and take on challenges that push my boundaries.</p>
         </div> 
         <div  className="mt-5" >
   <div className="d-flex align-items-center justify-content-center mb-5">
--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ function Home() {
   <About/>
   <Portfolio/>
   <Contact/>
-  <Footer/>
+  <Footer/> 
   </div>
   );
 }
