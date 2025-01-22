00ca742cffc7298aca70d3130e9c26eeb9265500
seif eddinne <159828511+seifeddinne0@users.noreply.github.com>
2025-01-22 20:18:29 +0100

    changing the animation of the skills section (Y)

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -12,7 +12,7 @@ function Skills(){
         <Horizontaline>
           <h1 className="mx-2 text-2xl montserrat text-center text-red-600">My Skills</h1>
         </Horizontaline>     
-        <motion.div initial={{x:-100,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:1}} className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:1}} className="flex flex-wrap items-center justify-center gap-4">
         <Skillscard srcImg="/nextJs.webp"altImg="Next" />
         <div className="rounded-2xl border-2 border-neutral-600 p-4">
           <motion.div initial={{scale:0.8}} animate={{scale:1.2}} transition={{duration:1,repeat:Infinity,repeatType:"reverse"}} >
